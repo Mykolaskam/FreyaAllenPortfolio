6a94bab533256aa16456e94b59d169f3519c662f
Mykolas Kaminskas <mykolaskam@gmail.com>
2018-11-04 18:01:22 +0000

    Created menu, Logo with animation, "work" page with basic elements and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-console.log("hello");
+
+window.addEventListener("scroll", function() {
+  const logo = document.getElementById("logo");
+  if (window.scrollY > 200) {
+    logo.classList.add("hidden");
+  } else {
+    logo.classList.remove("hidden");
+  }
+});
+
+document.getElementById("menu").addEventListener("click", function() {
+  const overlay = document.getElementById("overlay");
+  overlay.classList.add("open");
+});
+
+document.getElementById("close").addEventListener("click", function() {
+  const overlay = document.getElementById("overlay");
+  overlay.classList.remove("open");
+});
+
