0d182abf31ae160928c277a14d648d8aad88b613
Mykolas Kaminskas <mykolaskam@gmail.com>
2018-11-04 20:16:48 +0000

    menu improvements

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
 
 window.addEventListener("scroll", function() {
   const logo = document.getElementById("logo");
-  if (window.scrollY > 200) {
+  const nav = document.getElementById("navbar");
+  
+  if (window.scrollY > 400) {
+    nav.classList.add("cream");
+  } else {
+    nav.classList.remove("cream");
+  }
+
+  if (window.scrollY > 100) {
     logo.classList.add("hidden");
   } else {
     logo.classList.remove("hidden");
   }
+
+
 });
 
 document.getElementById("menu").addEventListener("click", function() {
