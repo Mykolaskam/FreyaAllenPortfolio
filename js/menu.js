d53ba689c665e03fbd953fabc2cf065a68ba4841
Mykolas Kaminskas <mykolaskam@gmail.com>
2018-12-27 18:00:09 +0000

    chabged button style, minor change to colors, started about page

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("menu").addEventListener("click", function () {
-    const overlay = document.getElementById("overlay");
-    overlay.classList.add("open");
-  });
-  
-  document.getElementById("close").addEventListener("click", function () {
-    const overlay = document.getElementById("overlay");
-    overlay.classList.remove("open");
-  });
--- js/work.js
+++ js/work.js
@@ -1,32 +1,80 @@
 window.addEventListener("scroll", function () {
-    
-    const body = document.getElementById("body");
-    const logo = document.getElementById("logo");
-    const menu = document.getElementById("menu");
-    const nav = document.getElementById("navbar");
-    const titles = document.getElementById("titles");
 
-    
-  
-    if (window.scrollY > 300) {
+  const body = document.getElementById("body");
+  const logo = document.getElementById("logo");
+  const menu = document.getElementById("menu");
+  const nav = document.getElementById("navbar");
+  const titles = document.getElementById("titles");
+  const borderbox = document.getElementById("borderbox");
+
+
+  if (window.scrollY > 300) {
+    if (typeof (logo) != 'undefined' && logo != null) {
       logo.classList.add("white");
+    }
+
+    if (typeof (menu) != 'undefined' && menu != null) {
       menu.classList.add("white");
+    }
+
+    if (typeof (body) != 'undefined' && body != null) {
       body.classList.add("black");
+    }
+
+    if (typeof (nav) != 'undefined' && nav != null) {
       nav.classList.add("dark");
+    }
+
+    if (typeof (borderbox) != 'undefined' && borderbox != null) {
+      borderbox.classList.add("black-border");
+    }
+
+    if (typeof (titles) != 'undefined' && titles != null) {
       titles.classList.add("scaled");
-    } else {
+    }
+
+  } else {
+
+    if (typeof (logo) != 'undefined' && logo != null) {
       logo.classList.remove("white");
+    }
+
+    if (typeof (menu) != 'undefined' && menu != null) {
       menu.classList.remove("white");
+    }
+
+    if (typeof (body) != 'undefined' && body != null) {
       body.classList.remove("black");
+    }
+
+    if (typeof (nav) != 'undefined' && nav != null) {
       nav.classList.remove("dark");
+    }
+
+    if (typeof (borderbox) != 'undefined' && borderbox != null) {
+      borderbox.classList.remove("black-border");
+    }
+
+    if (typeof (titles) != 'undefined' && titles != null) {
       titles.classList.remove("scaled");
     }
+  }
 
-    if (window.scrollY > 100) {
-      logo.classList.add("hidden");
-    } else {
-      logo.classList.remove("hidden");
-    }
-  
-  });
+  if (window.scrollY > 100) {
+    logo.classList.add("hidden");
+  } else {
+    logo.classList.remove("hidden");
+  }
+
+});
+
+document.getElementById("menu").addEventListener("click", function () {
+  const overlay = document.getElementById("overlay");
+  overlay.classList.add("open");
+});
+
+document.getElementById("close").addEventListener("click", function () {
+  const overlay = document.getElementById("overlay");
+  overlay.classList.remove("open");
+});
 
